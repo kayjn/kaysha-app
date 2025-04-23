a3778663929837908ef8a6e6b6839290b90c663c
kayjn <kaysha.jasmine02@gmail.com>
2025-04-23 15:31:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("🎈 hai barudawgh")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.image("dhalf 2025-04-21 140653.436.jpeg")
